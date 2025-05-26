8dcf7cd9f24c30e8a218f15d44a69f38c8b7305a
Adib <adibaziz281296@gmail.com>
2025-05-26 13:33:05 +0800

    feat: token validation

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -8,5 +9,27 @@ const api = axios.create({
   },
   withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use((response) => response, (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove("token");
+      window.location.href = "/login";
+
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
